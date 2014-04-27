9afb79931462ec07dc3ad2f64829e8a861801722
codelectron <codelectron@gmail.com>
2014-04-27 19:00:51 +0000

    Added Serial support and example

---
+++ examples/Serial/SerialApp.cpp
@@ -0,0 +1,34 @@
+/*
+Author: Krishna Pattabiraman
+Date: 27/04/2014
+Contact:www.codelectron.com
+This example opens the serial interface ttyACM0 at baudrate 9600 and prints out when there is any data available.
+This was tested with Arduino connected to Raspberry pi printing some message periodically
+*/
+
+#include <stdio.h>
+#include <Serial.h>
+char buffer[100];
+Serial serial;
+void setup()
+{
+  serial.Open("/dev/ttyACM0",9600);
+  
+}
+
+
+void loop()
+{
+	int len;
+	if(serial.available())
+	{
+		len = serial.available();
+	
+		serial.Read(buffer, len, 0);
+
+		printf("%s\n",buffer);
+	}
+	printf("This is setup\n");
+	sleep(1);
+
+}
